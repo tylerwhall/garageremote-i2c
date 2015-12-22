764f2e231438b606609ca35a76ae398fa00a6db2
Tyler Hall <tylerwhall@gmail.com>
2015-12-21 22:10:09 -0500

    blinks red

--- rf.asm
+++ rf.asm
@@ -1,22 +1,27 @@
 	processor 12f529t39a
 	include "p12f529t39a.inc"
-        __CONFIG _WDT_OFF & _OSC_INTRC & _IOSCFS_8MHz & _MCLRE_OFF
+        __CONFIG _WDT_OFF & _OSC_INTRC & _IOSCFS_8MHz & _MCLRE_OFF & _CP_DISABLE & _CPDF_OFF
 
+RED_LED EQU 4
         ; set prescaler to max
         movlw   NOT_RBPU & NOT_RBWU & b'111'
         option
+        bcf     PORTB, RED_LED
         ; all pins to input except 0
-        movlw   0xfe
-        tris    w
+
+        movlw   0xff
+        tris    PORTB
 
 delay:
         ; test top bit of timer
         btfss   TMR0, 7
         goto    timer_low
-        bcf     PORTB, 1
+        andlw   0xff & ~(1 << RED_LED)
+        tris    PORTB
         goto    delay
 
 timer_low:
-        bsf     PORTB, 0
+        iorlw   (1 << RED_LED)
+        tris    PORTB
         goto    delay
         end
